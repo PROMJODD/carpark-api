316e9b562d077aefa853687169c5f6ca23f4dec6
pjames <pjames@gmail.com>
2023-09-06 22:30:26 +0700

    Added Kafka producer

---
+++ LPR-API/Models/MLPRResult.cs
@@ -0,0 +1,10 @@
+
+namespace Prom.LPR.Api.Models
+{
+    public class MLPRResult
+    {
+        public int? Status { get; set; }
+        public string? Message { get; set; }
+        public MVehicle? Data { get; set; }
+    }
+}
