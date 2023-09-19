ab4fa245f44db1b2073009f678dd730d5a73a076
pjames <pjames@gmail.com>
2023-09-19 17:48:49 +0700

    Added Unit Testing

--- LPR-API.TEST/Database/DbContextMock.cs
+++ LPR-API.TEST/Database/DbContextMock.cs
@@ -8,6 +8,9 @@ public static class DbContextMock
 {
     public static DbSet<T> GetQueryableMockDbSet<T>(List<T> sourceList) where T: class 
     {
+        var addAction = (T s) => sourceList.Add(s);
+        var addActionAsync = (T s, CancellationToken t) => { sourceList.Add(s); };
+
         var queryable = sourceList.AsQueryable().BuildMock();
         var dbSet = new Mock<DbSet<T>>();
 
@@ -15,7 +18,11 @@ public static class DbContextMock
         dbSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
         dbSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
 
-        dbSet.Setup(d => d.Add(It.IsAny<T>())).Callback<T>((s) => sourceList.Add(s));
+        dbSet.Setup(d => d.Add(It.IsAny<T>())).Callback(addAction);
+
+        dbSet.Setup(d => d.AddAsync(It.IsAny<T>(), It.IsAny<CancellationToken>()))
+            .Callback(addActionAsync);
+            //.Returns((T model, CancellationToken token) => Task.FromResult((T) null!));
 
         return dbSet.Object;
     }
