546236b7b58fa9154e1ccff2e47e72e2dc4940da
pjames <pjames@gmail.com>
2023-09-28 07:21:22 +0700

    Fixed per Sonarqube

--- LPR-API/Database/Repositories/Role/RoleRepository.cs
+++ LPR-API/Database/Repositories/Role/RoleRepository.cs
@@ -11,17 +11,10 @@ namespace Prom.LPR.Api.Database.Repositories
 
         public IEnumerable<MRole> GetRolesList(string rolesList)
         {
-            try
-            {
-                var list = rolesList.Split(',').ToList();
-                var arr = context!.Roles!.Where(p => list.Contains(p.RoleName!)).ToList();
+            var list = rolesList.Split(',').ToList();
+            var arr = context!.Roles!.Where(p => list.Contains(p.RoleName!)).ToList();
 
-                return arr;
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            return arr;
         }
     }
 }
