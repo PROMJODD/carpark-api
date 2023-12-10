ea05ea0d2bbd16b7b85969494460639344414879
pjames <pjames@gmail.com>
2023-12-10 12:57:18 +0700

    Added debug message

--- scripts/test-upload-api.bash
+++ scripts/test-upload-api.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. ./export-dev.bash
+. ./export-prod.bash
 FILE_TO_UPLOAD="/d/LPR/Feb_2_2020/2020-02-18/0715327008_1R2005.jpg"
 
 REF_ID=$(date +"%s")
@@ -10,7 +10,7 @@ BRANCH_ID=AB001
 EQUIPMENT_ID=CCTV-001
 UPLOADED_SIZE=$(wc -c < ${FILE_TO_UPLOAD})
 
-curl -s -X POST ${UPLOAD_ENDPOINT} -u ${AUTH_USER}:${AUTH_PASSWORD_UPLOADER} -F "image=@${FILE_TO_UPLOAD}" \
+curl -v -X POST ${UPLOAD_ENDPOINT} -u ${AUTH_USER}:${AUTH_PASSWORD_UPLOADER} -F "image=@${FILE_TO_UPLOAD}" \
    -H "Prom-Ref-ID: ${REF_ID}" \
    -H "Prom-Upload-Path: ${UPLOADED_PATH}" \
    -H "Prom-Upload-Size: ${UPLOADED_SIZE}" \
