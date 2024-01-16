9f3b196caee7e3862d687d149c24582c240f8e05
pjames <pjames@gmail.com>
2024-01-16 11:26:44 +0700

    Setup script for testing

--- scripts/test-get-files-uploaded.bash
+++ scripts/test-get-files-uploaded.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-. ./export-dev.bash
+. ./export-prod.bash
 
 # Must use echo -n to remove new line !!!
 BASE64_AUTH_JWT=$(echo -n ${AUTH_JWT} | base64 -w0)
 
-curl -v -X GET "${ENDPOINT_GET_FILES}" \
+curl -s -X GET "${ENDPOINT_GET_FILES}" \
     -H "Authorization: Bearer ${BASE64_AUTH_JWT}"
     #-u ${AUTH_USER}:${AUTH_PASSWORD}
 
-#curl -s -X GET "${ENDPOINT_GET_FILES}Count" \
-#    -H "Authorization: Bearer ${BASE64_AUTH_JWT}"
-#    #-u ${AUTH_USER}:${AUTH_PASSWORD}
+curl -s -X GET "${ENDPOINT_GET_FILES}Count" \
+    -H "Authorization: Bearer ${BASE64_AUTH_JWT}"
+    #-u ${AUTH_USER}:${AUTH_PASSWORD}
