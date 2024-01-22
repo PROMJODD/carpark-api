a42faf7de0e1215bfc18054fc52850d8383f431c
pjames <pjames@gmail.com>
2024-01-22 21:48:35 +0700

    Test script

--- scripts/test-get-files-uploaded.bash
+++ scripts/test-get-files-uploaded.bash
@@ -5,9 +5,9 @@
 # Must use echo -n to remove new line !!!
 BASE64_AUTH_JWT=$(echo -n ${AUTH_JWT} | base64 -w0)
 
-#curl -s -X GET "${ENDPOINT_GET_FILES}" \
-#    -u ${AUTH_USER}:${AUTH_PASSWORD_VIEWER}
-#    #-H "Authorization: Bearer ${BASE64_AUTH_JWT}"
+curl -s -X GET "${ENDPOINT_GET_FILES}" \
+    -u ${AUTH_USER}:${AUTH_PASSWORD_VIEWER}
+    #-H "Authorization: Bearer ${BASE64_AUTH_JWT}"
 
 
 curl -s -X GET "${ENDPOINT_GET_FILES}Count" \
